ef73b5be574cc89c5b69b549a769d7f8fb14c821
=raj-maharjan99 <=rjuallone@gmail.com>
2024-02-13 00:46:44 -0600

    make it more responsive using tailwind

--- src/components/GitHub.jsx
+++ src/components/GitHub.jsx
@@ -27,36 +27,45 @@ function GitHub({ userName }) {
   );
   return (
     <>
-      <div className=" min-h-[200px] w-[1280px] px-5 mt-[100px]  m-auto   flex  justify-center ">
-        <div className="w-[500px]  flex flex-col gap-3">
-          <div className="">
-            <img className="w-100 rounded-3xl" src={data.avatar_url} alt="" />
-          </div>
-          <div className="flex flex-col justify-start">
-            <b className="text-[25px] font_family">{data.name}</b>
+      <div className="w-full px-5 pb-[30px] mt-20 max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-8">
+        <div className="flex flex-col items-center md:items-start">
+          <img
+            className="w-32 h-32 md:w-48 md:h-48 rounded-full mb-4 md:mb-0"
+            src={data.avatar_url}
+            alt=""
+          />
+          <div className="text-center md:text-left">
+            <b className="text-2xl md:text-3xl font-semibold">{data.name}</b>
             <p>
-              <b className="text-[]">Username</b> :- {data.login}
+              <b className="font-semibold">Username:</b> {data.login}
             </p>
             <p>
-              <b>Followers</b> :- {data.followers}
+              <b>Followers:</b> {data.followers}
             </p>
             <span>
-              <b>Created At :- </b>
-              {createdFormatDate}
+              <b>Created At:</b> {createdFormatDate}
             </span>
             <span>
-              <b>Updated At :- </b>
-              {updatedFormatDate}
+              <b>Updated At:</b> {updatedFormatDate}
             </span>
           </div>
         </div>
-        {/* .......... */}
-        <div className="w-[500px] flex flex-col gap-2 text-center">
-          <u className="text-[40px] font_style pb-5">REPOSITORIES</u>
-          <div className="flex flex-wrap justify-center gap-4 ">
+        <div className="flex flex-col items-center md:items-start">
+          <u className="text-4xl max-sm:text-[20px] font-semibold pb-5">
+            REPOSITORIES
+          </u>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             {repo.slice(0, 9).map((item) => (
-              <a href={item.html_url} target="_blank">
-                <div className="size ">{item.name} </div>
+              <a
+                key={item.id}
+                href={item.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:underline-none"
+              >
+                <div className="bg-gray-200 p-4   text-center hover:bg-orange-400 hover:text-white  rounded-lg">
+                  {item.name}
+                </div>
               </a>
             ))}
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,53 @@
 import React, { useState } from "react";
 import GitHub from "./GitHub";
+
 export default function Header() {
   const [search, setSearch] = useState("");
   const [userName, setUserName] = useState("");
-  const profileLink = "https://github.com/&{userName}";
+
   const searchBtn = () => {
     setUserName(search);
     setSearch("");
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
     <>
-      <div className="bg-[#0f0a2a] text-[30px] flex  items-center justify-evenly gap-5 font_family text-white py-5 text-center nav_shadow">
-        <a href={`https://github.com/&{userName}`}>
-          <div className="logo">GIT HUB PROFILE</div>
-        </a>
+      <div
+        className="bg-[#3e3e48] max-sm:bg-slate-700 text-[20px] flex 
+      max-sm:w-[400px] max-sm:flex max-sm:flex-col max-sm:gap-[40px] 
+       items-center justify-evenly gap-5 font_family text-white py-5 text-center nav_shadow"
+      >
+        <div className="logo max-sm:border w-[50%] ">
+          {" "}
+          <a target="_blank" href={`https://github.com/${userName}`}>
+            <img
+              className="w-100 "
+              src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
+              alt=""
+            />
+          </a>
+        </div>
+
         <form
           onSubmit={handleSubmit}
-          className="flex items-center justify-center gap-6"
+          className="flex max-sm:w-[350px] max-sm:flex-col items-center justify-center gap-6"
           action=""
         >
           <div>
             <input
-              placeholder="write your username here"
-              className="px-3  h-[60px] w-[450px]  text-center text-slate-600"
+              placeholder="write here...."
+              className="px-3  max-sm:w-[300px] h-[60px] w-[450px] border rounded-xl  text-center text-slate-600"
               type="text"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
           </div>
           <div className="border px-3">
-            <button className="" onClick={searchBtn}>
+            <button className="search-btn" onClick={searchBtn}>
               Search
             </button>
           </div>
@@ -42,5 +57,3 @@ export default function Header() {
     </>
   );
 }
-
-// export default Header;
